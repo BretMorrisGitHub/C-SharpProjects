87003298ef3ed5c5965811848c7b4919fb0e6c48
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-06-23 16:31:16 -0700

    Update

---
+++ Basic_C#_Programs/WelcomeScreen.cs
@@ -0,0 +1,7 @@
+using System;
+
+class WelcomeScreen {
+    static void Main(){
+        Console.WriteLine("Welcome to Acme Accounting Systems \l Remember, we're \"accounting\" on you!")
+    }
+}
---
+++ Basic_C#_Programs/program.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+namespace VariablesAndDataTypes
+{
+	class Program
+    {
+		static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("What is you name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
