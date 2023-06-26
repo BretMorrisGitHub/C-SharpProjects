e20f8493a18da22183be25152515de51ee736c66
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-06-26 14:54:55 -0700

    ConsoleMath update

---
+++ ConsoleMath/ConsoleMath/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This block of code uses the multiplication operator '*' to multiply a number given from the user stored in variables
+            Console.WriteLine("Input a number to be multiplied by 50:");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int sum1 = input1 * 50;
+            
+            // This block of code uses string concatenation with the variables to write a sentence to the user that they can understand
+            Console.WriteLine(input1 + " multiplied by 50 is " + sum1);
+            Console.ReadLine();
+
+            // This block of code uses the addition operator '+' to add a number given from the user stored in variables
+            Console.WriteLine("Input a number that will be add 25 to it:");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int sum2 = input2 + 25;
+
+            // This block of code uses string concatenation with the variables to write a sentence to the user that they can understand
+            Console.WriteLine(input2 + " plus 25 equals " + sum2);
+            Console.ReadLine();
+
+            // This block of code uses the division operator '/' to divide a number given from the user stored in variables
+            Console.WriteLine("Input a number that will be divided by 12.5:");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            double sum3 = input3 / 12.5;
+
+            // This block of code uses string concatenation with the variables to write a sentence to the user that they can understand
+            Console.WriteLine(input3 + " divided by 12.5 equals " + sum3);
+            Console.ReadLine();
+
+            // This block of code uses a comparative operator to find out is the inputed number is greater than 50
+            Console.WriteLine("Input a number to be checked if it is greater than 50:");
+            int input4 = Convert.ToInt32(Console.ReadLine());
+            bool compare = input4 > 50;
+
+            // This block of code uses string concatenation with the variables to write a sentence to the user that they can understand
+            Console.WriteLine(compare);
+            Console.ReadLine();
+
+            // This block of code uses the modulus operator '%' to divide a number given from the user stored in variables, and only bring back the remainder
+            Console.WriteLine("Input a number that will be divided by 7, then the remainder will be outputed:");
+            int input5 = Convert.ToInt32(Console.ReadLine());
+            int sum4 = input5 % 7;
+
+            // This block of code uses string concatenation with the variables to write a sentence to the user that they can understand
+            Console.WriteLine(input5 + " divided by 7, with a remainder of " + sum4);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Using Addition in C#
+            int num1 = 5;
+            int num2 = 12;
+            int addTotal = num1 + num2;
+            Console.WriteLine(addTotal);
+            Console.ReadLine();
+
+            // Using Subtraction in C#
+            int num3 = 24;
+            int num4 = 13;
+            int addTotal1 = num3 - num4;
+            Console.WriteLine(addTotal1);
+            Console.ReadLine();
+
+            // Using multiplication in C#
+            int num5 = 24;
+            int num6 = 13;
+            int addTotal2 = num5 * num6;
+            Console.WriteLine(addTotal2);
+            Console.ReadLine();
+
+            // Using division in C#
+            int num7 = 2400;
+            int num8 = 13;
+            int addTotal3 = num7 / num8;
+            Console.WriteLine(addTotal3);
+            Console.ReadLine();
+
+            //Using string concatenation in C#
+            string fname = "Bret";
+            string lname = "Morris";
+            Console.WriteLine(fname + lname);
+            Console.ReadLine();
+        }
+    }
+}
