6f0b7f0ce08b7c0733bf72ff507e6cb0f141592b
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-06-28 11:38:59 -0700

    BranchingAssign commit

---
+++ Basic_C#_Programs/BranchingAssign/BranchingAssign/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Please enter package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int total = width + height + length;
+
+            if (total > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            decimal quote = Convert.ToDecimal(width * height * length * weight / 100);
+
+            Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorChall/OperatorChall/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorChall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////if (true != false)
+            //////{
+            //////    Console.WriteLine("True does not equal false!!!!");
+            //////    Console.ReadLine();
+            //////}
+            ///
+
+            if (12 > 4)
+            {
+                Console.WriteLine("12 is greater than 4!!!");
+                Console.ReadLine();
+            }
+
+            if (12 <= 63214)
+            {
+                Console.WriteLine("12 is lesser than or equal to 63214");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
