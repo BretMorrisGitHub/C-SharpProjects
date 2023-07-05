f8ff2eef3112673ff31b14fe7ce81005ab65f95e
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-05 16:28:07 -0700

    CallingMethodsAssign Commit

---
+++ Basic_C#_Programs/CallingMethodsAssign/CallingMethodsAssign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code creates an instance of the math class to be used
+            math obj = new math();
+            // Asks for user input, to be stored in a variable
+            Console.WriteLine("Input your number to do math operations on:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            // Uses the methods defined in the class with the user input as the parameter
+            Console.WriteLine(obj.multiplication(input));
+            Console.WriteLine(obj.division(input));
+            Console.WriteLine(obj.addition(input));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsAssign/CallingMethodsAssign/math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssign
+{
+    public class math
+    {
+        // Defining three methods using various operators to do math operations
+        public int multiplication (int num)
+        {
+            return num * 7;
+        }
+
+        public int division (int num)
+        {
+            return num / 7;
+        }
+
+        public int addition (int num)
+        {
+            return num + 7;
+        }
+    }
+}
