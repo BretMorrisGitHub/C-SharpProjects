62cd752153da11588b81338755e7c541c92dff37
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-17 14:02:23 -0700

    VoidMethod commit

---
+++ Basic_C#_Programs/VoidMethodAssign/VoidMethodAssign/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssign
+{
+    class Math
+    {
+        // Defines the method in this class
+        public void operation(int num1, int num2)
+        {
+            // Does a math operation on the first given parameter
+            int answer = num1 * 3;
+            
+            // Writes to the console the answer to the math operation in a way the user can understand
+            Console.WriteLine(num1 + " times 3 is equal to " + answer);
+            // Wtires to the console the second parameter given
+            Console.WriteLine(num2 + " is the second number given.");
+            // Keeps the lines on the console 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidMethodAssign/VoidMethodAssign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiates the class 'Math'
+            Math obj = new Math();
+            // Calls the method in the class passing in two numbers
+            obj.operation(23, 46);
+
+            // Asks user for input of two numbers, and stores them as variables named 'input1', and 'input2'.
+            Console.WriteLine("Please enter a number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            // Calls the method with the two variables as the parameters
+            obj.operation(input1, input2);
+        }
+    }
+}
