5445a6b39fc1731181bbc5af5c5ac90e1426aa1d
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-18 11:19:43 -0700

    ClassAssign commit

---
+++ Basic_C#_Programs/ClassAssign/ClassAssign/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssign
+{
+    class Math
+    {
+        // Defines the method 'operation' as a void method with one parameter, and outputs an integer from a math operation
+        public void operation(int num)
+        {
+            int output = num / 2;
+            Console.WriteLine(output);
+        }
+
+        // Defines an overloaded static void method that has requires two arguments, and has two outputs
+        public static void operation(int num1, int num2)
+        {
+            int output1 = num1 / 3;
+            int output2 = num2 / 3;
+            Console.WriteLine(num1 + " divided by 3 equals: " + output1);
+            Console.WriteLine(num2 + " divided by 3 equals: " + output2);
+        }
+    }
+
+}
---
+++ Basic_C#_Programs/ClassAssign/ClassAssign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiates the class into an object
+            Math obj = new Math();
+            // Asks the user for input, and stores it inside a variable
+            Console.WriteLine("Please enter a number to be divided by 2:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            // Calls the method 'operation' with the user input as the argument
+            obj.operation(input);
+            // Keeps the console from closing automatically 
+            Console.ReadLine();
+
+            // Asks the user for more number inputs, and stores them in variables
+            Console.WriteLine("Please enter a number to be divided 3: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number to be divided 3: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            // Calls the method using the class name, as it is set to static
+            Math.operation(input1, input2);
+            Console.ReadLine();
+        }
+    }
+}
