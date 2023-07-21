71ec893fb8c0253477a90bfc578292ae2bfcd79b
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-21 11:39:56 -0700

    StructAssign commit

---
+++ Basic_C#_Programs/StructAssign/StructAssign/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssign
+{
+    // Creates a struct called 'Number'
+    public struct Number
+    {
+        // Creates the property 'Amount'
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/StructAssign/StructAssign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates an object of data type 'Number', and assigns a value to the 'Amount' property
+            Number number = new Number();
+            number.Amount = 4.3m;
+            // Wrties the value assigned to 'Amount' to the console, and leaves it on the screen
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
