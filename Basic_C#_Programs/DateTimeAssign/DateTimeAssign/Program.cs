c53b654a3ba0ae4c89fa0293a8af6e454fc4ba59
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-24 16:43:25 -0700

    DateTimeAssign commit

---
+++ Basic_C#_Programs/DateTimeAssign/DateTimeAssign/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gets the current time with the '.Now' method, and stores it in a variable for use
+            DateTime currentTime = DateTime.Now;
+            // Writes the current time to the console and keeps it on the screen
+            Console.WriteLine(currentTime);
+            Console.ReadLine();
+            
+            // Asks the user for a number input, and stores it in a variable
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            // Creates a new DateTime variable that uses the 'AddHours' method to add however much the value in the variable 'input' is, to the hours stored
+            DateTime newTime = currentTime.AddHours(input);
+            // Prints the value inside the 'newTime' variable, and keeps it on the screen
+            Console.WriteLine(newTime);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/InputAssign/InputAssign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be added to a log: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\legol\logs\log.txt", true))
+            {
+                file.WriteLine(input);
+            }
+        }
+    }
+}
