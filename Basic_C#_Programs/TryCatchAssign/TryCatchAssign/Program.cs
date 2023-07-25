5fd11f420fe33e8bd6869021b8abf984b05fa101
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-07-25 15:24:05 -0700

    update

---
+++ Basic_C#_Programs/TryCatchAssign/TryCatchAssign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age");
+                
+            }
+            catch 
+            {
+
+            }
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            DateTime currentTime = DateTime.Now;
+            DateTime birthYear = currentTime.AddYears(-age);
+
+            Console.WriteLine(birthYear + "You were either born this specified year, or the year before.");
+            Console.ReadLine();
+        }
+    }
+}
