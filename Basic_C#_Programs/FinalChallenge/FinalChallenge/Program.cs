d46b6803f83a2d25a9b32fe962c12fb97345acb3
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-08-01 18:22:17 -0700

    FinalChallenge Commit

---
+++ Basic_C#_Programs/FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var school = new SchoolContext())
+            {
+                var student = new Student();
+                student.FirstName = "Joe";
+                student.LastName = "Morley";
+                student.Grade = 11;
+
+                school.Students.Add(student);
+                school.SaveChanges();
+                
+            }
+        }
+    }
+}
